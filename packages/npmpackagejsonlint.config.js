7f659305b6aa142b8b31781d62393a6cc69168c2
Mikael Korpela <mikael@ihminen.org>
2019-10-04 17:30:18 +0300

    Monorepo: add package.json linter

---
+++ packages/npmpackagejsonlint.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	// See https://www.npmjs.com/package/@wordpress/npm-package-json-lint-config
+	extends: '@wordpress/npm-package-json-lint-config',
+	rules: {
+		'prefer-property-order': 'off',
+		'require-author': 'error',
+		'require-bugs': 'off',
+		'require-homepage': 'off',
+		'require-keywords': 'off',
+		'require-license': 'error',
+		'require-name': 'error',
+		'require-version': 'error',
+		'valid-values-author': [ 'error', [ 'Automattic, Inc' ] ],
+		'valid-values-license': [ 'error', [ 'GPL-2.0-or-later' ] ],
+		'valid-values-name-scope': [ 'error', [ '@automattic' ] ],
+	},
+	overrides: [
+		{
+			patterns: [
+				'./packages/eslint-config-wpcalypso/package.json',
+				'./packages/eslint-plugin-wpcalypso/package.json',
+				'./packages/i18n-calypso/package.json',
+				'./packages/photon/package.json',
+				'./package.json',
+			],
+			rules: {
+				'valid-values-name-scope': 'off',
+			},
+		},
+		{
+			patterns: [ './packages/material-design-icons/package.json' ],
+			rules: {
+				'valid-values-license': 'off',
+			},
+		},
+	],
+};
